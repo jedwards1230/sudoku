599440e0c162c5b69050b8f103a3baf69ea7c00b
Justin Edwards <justinedwards1230@gmail.com>
2022-03-28 11:05:30 -0400

    remove unused code

--- sudoku.py
+++ sudoku.py
@@ -72,23 +72,3 @@ class SudokuGenerator:
                 id += 1
             puzzle.append(new_row)
         return puzzle
-    
-    def shortSudokuSolve(self, board):
-        size    = len(board)
-        block   = int(size**0.5)
-        board   = [n for row in board for n in row ]      
-        span    = { (n,p): { (g,n)  for g in (n>0)*[p//size, size+p%size, 2*size+p%size//block+p//size//block*block] }
-                    for p in range(size*size) for n in range(size+1) }
-        empties = [i for i,n in enumerate(board) if n==0 ]
-        used    = set().union(*(span[n,p] for p,n in enumerate(board) if n))
-        empty   = 0
-        while empty>=0 and empty<len(empties):
-            pos        = empties[empty]
-            used      -= span[board[pos],pos]
-            board[pos] = next((n for n in range(board[pos]+1,size+1) if not span[n,pos]&used),0)
-            used      |= span[board[pos],pos]
-            empty     += 1 if board[pos] else -1
-            if empty == len(empties):
-                solution = [board[r:r+size] for r in range(0,size*size,size)]
-                yield solution
-                empty -= 1
