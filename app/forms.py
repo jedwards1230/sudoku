5723c972002172214286a8c2de0a86190b3f51a0
Justin Edwards <justinedwards1230@gmail.com>
2022-03-28 20:24:33 -0400

    grid scales up to 16x16

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-# TODO: Make this able to scale with board size
 class PuzzleForm(forms.Form):
     col_0 = forms.CharField(label='0', widget=forms.TextInput(attrs={'readonly':'readonly'}))
     col_1 = forms.CharField(label='1', widget=forms.TextInput(attrs={'readonly':'readonly'}))
@@ -11,13 +10,11 @@ class PuzzleForm(forms.Form):
     col_6 = forms.CharField(label='6', widget=forms.TextInput(attrs={'readonly':'readonly'}))
     col_7 = forms.CharField(label='7', widget=forms.TextInput(attrs={'readonly':'readonly'}))
     col_8 = forms.CharField(label='8', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    
-'''    cell_9 = forms.CharField(label='9', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_10 = forms.CharField(label='10', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_11 = forms.CharField(label='11', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_12 = forms.CharField(label='12', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_13 = forms.CharField(label='13', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_14 = forms.CharField(label='14', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_15 = forms.CharField(label='15', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_16 = forms.CharField(label='16', widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    cell_17 = forms.CharField(label='17', widget=forms.TextInput(attrs={'readonly':'readonly'}))'''
+    col_9 = forms.CharField(label='9', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_10 = forms.CharField(label='10', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_11 = forms.CharField(label='11', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_12 = forms.CharField(label='12', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_13 = forms.CharField(label='13', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_14 = forms.CharField(label='14', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_15 = forms.CharField(label='15', widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    col_16 = forms.CharField(label='16', widget=forms.TextInput(attrs={'readonly':'readonly'}))
