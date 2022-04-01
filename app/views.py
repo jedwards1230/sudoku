3ba8dc03bef05cbc701599b1672ef35054087efa
Justin Edwards <justinedwards1230@gmail.com>
2022-04-01 00:33:06 -0400

    track elapsed time

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 
 import app.sudoku_utils as su
@@ -7,8 +8,8 @@ def index(request):
     context = {
         'config': {
             'size': 3,
-            'difficulty': 4,
-        }
+            'difficulty': 0,
+        },
     }
     
     if request.method == 'POST':
@@ -17,18 +18,31 @@ def index(request):
         
         # check submitted puzzle
         if request.POST.get('submit_puzzle') == '1':
+            time = int(datetime.now().timestamp())
+            start_time = int(request.POST.get('time')) or None
+            delta_time = time - start_time
             puzzle, edits = su.to_python(request.POST)
             
-            if puzzle and edits:
-                context['win'] = su.check_solution(puzzle, context['config']['size'])
+            if puzzle and edits and delta_time:
+                print('post', int(request.POST.get('time')))
+                if su.check_solution(puzzle, context['config']['size']):
+                    context['win'] = True
+                    context['elapsed_time'] = str(delta_time)
+                    print('elapsed', context['elapsed_time'])
+
                 formset = su.new_formset(context['config']['size'], context['config']['difficulty'], puzzle, edits)
             else:
                 context['invalid_submission'] = 'There was an error validating the board.'
         # generate new puzzle
         elif request.POST.get('new_puzzle') == '1':
             formset = su.new_formset(context['config']['size'], context['config']['difficulty'])
+            context['time'] = int(datetime.now().timestamp())
+            print('new', context['time'])
+            
     else:
         formset = su.new_formset(context['config']['size'], context['config']['difficulty'])
+        context['time'] = int(datetime.now().timestamp())
+        print('init', context['time'])
     
     context['puzzle'] = formset
         
