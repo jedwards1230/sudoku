cbdb52f4cf60db4e35e95c2ae1875a6aba9e266d
jedwards1230 <justinedwards1230@gmail.com>
2022-04-02 17:19:29 -0400

    Update models.py

--- sudoku/models.py
+++ sudoku/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-class Puzzle(models.Model):
+class Puzzle(models.Model):    
+    puzzle = models.TextField(null=True)
+    
     size = models.IntegerField()
     difficulty = models.IntegerField()
     
-    row1 = models.CharField(max_length=9)
-    row2 = models.CharField(max_length=9)
-    row3 = models.CharField(max_length=9)
-    row4 = models.CharField(max_length=9)
-    row5 = models.CharField(max_length=9)
-    row6 = models.CharField(max_length=9)
-    row7 = models.CharField(max_length=9)
-    row8 = models.CharField(max_length=9)
-    row9 = models.CharField(max_length=9)
-    
-    created_time = models.DateTimeField('time published')
-    elapsed_time = models.DateTimeField('time finished')
+    created_time = models.DateTimeField('time published', null=True)
+    elapsed_time = models.DateTimeField('time finished', null=True)
