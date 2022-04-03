b2c45a34dfdb96ad58e0a57a87b33781fcbf0966
Justin Edwards <justinedwards1230@gmail.com>
2022-04-03 02:29:29 -0400

    clean unused code

--- sudoku/views.py
+++ sudoku/views.py
@@ -3,42 +3,40 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from sudoku.sudoku import SudokuGenerator
-
-import sudoku.sudoku_utils as su
+from sudoku.sudoku_utils import check_solution
 
 
 def index(request):
-    context = {
-        'size': 3,
-        'difficulty': 4,
-        'time': int(datetime.now().timestamp()),
-    }
-    
     if request.method == 'POST':
-        context['size'] = int(request.POST.get('size'))
-        context['difficulty'] = int(request.POST.get('difficulty'))
-        
         # check submitted puzzle
         if request.POST.get('submit_puzzle'):
+            delta_time = int(datetime.now().timestamp()) - int(request.POST.get('time'))
+            context = {
+                'elapsed_time': str(delta_time),
+            }
+            
             puzzle = request.POST.getlist('puzzle[]')
-            
-            time = int(datetime.now().timestamp())
-            context['time'] = int(request.POST.get('time'))
-            delta_time = time - context['time']
-            context['elapsed_time'] = str(delta_time)
-            
             if puzzle:
-                context['win'] = su.check_solution(puzzle, context['size'])
-                
+                context['win'] = check_solution(puzzle, int(request.POST.get('size')))
                 return HttpResponse(json.dumps(context), content_type="application/json")
             else:
                 raise ValueError('Missing puzzle')
             
         # generate new puzzle
         elif request.POST.get('new_puzzle'):
+            context = {
+                'size': int(request.POST.get('size')),
+                'difficulty': int(request.POST.get('difficulty')),
+                'time': int(datetime.now().timestamp()),
+            }
             context['puzzle'] = SudokuGenerator(context['size'], context['difficulty']).board_puzzle
             
             return HttpResponse(json.dumps(context), content_type="application/json")
             
     else:
+        context = {
+            'size': 2,
+            'difficulty': 0,
+            'time': int(datetime.now().timestamp()),
+        }
         return render(request, 'sudoku/index.html', context)
